3839c0bd017479cd96478edf3a0a016b7d784dc9
jkh675 <rluo2018@gmail.com>
2026-04-16 14:59:07 +0800

    refactor: Remove old backend

--- apps/frontend/src/integrations/tanstack-query/api.ts
+++ apps/frontend/src/integrations/tanstack-query/api.ts
@@ -1,5 +1,5 @@
 import { env } from "#/env";
 import { treaty } from "@elysiajs/eden";
-import type { App } from "@shooting_sports_scoreboard/backend-v2";
+import type { App } from "@shooting_sports_scoreboard/backend";
 
 export const client = treaty<App>(env.VITE_BACKEND_ENDPOINT);
